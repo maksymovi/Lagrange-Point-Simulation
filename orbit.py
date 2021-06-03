aa0758b595a778b8fd2db0b0498cdcc2bf891075
Maksym Prokopovych <24415429+maksymovi@users.noreply.github.com>
2021-06-03 01:27:42 -0700

    Final code for this project

--- orbit.py
+++ orbit.py
@@ -14,52 +14,89 @@ from moviepy.editor import VideoClip
 
 #sampling frequency in hertz, steps the diffeq, bigger number is more physically accurate
 
-sampleFreq = 9000;
+sampleFreq = 900;
 
 #Gravity constant, increase for stronger gravity, or increase the mass
 
-Gconst = 100;
+Gconst = 1;
 
 #simulation time in seconds, length of video essentially
 
-simulationTime = 5;
+simulationTime = 120;
 
 #video steeings
 #ideally framerate should divide sampleFreq, it must not be greater than sampleFreq, code breaks if so
-framerate = 30;
+framerate = 60;
 frameheight = 1080
 framewidth = 1920
-videoname="normalorbit.mp4"
+videoname="finalorbit.mp4"
 
 #rendering settings
 #texture for sphere, currently purple.
-tex = Texture( Pigment( 'color', [1, 0, 1] ), Finish('ambient', 1.0)) #standard texture for sphere
-camera = Camera( 'location', [10, 10, 10], 'look_at', [0, 0, 0] ) #camera location
-light = LightSource( [2, 4, -3], 'color', [1, 1, 1] ) #light location and angle/color
+sunTex = Texture( Pigment( 'color', [1, 1, 1] ), Finish('ambient', 1.0)) #standard texture for sphere
+earthTex = Texture(Pigment('color', [0, 1, 0] ), Finish('ambient', 1.0))
+
+lagTex = Texture(Pigment('color', [0, 0, 1] ), Finish('ambient', 1.0))
+
+errLagTex = Texture(Pigment('color', [1, 1, 0] ), Finish('ambient', 1.0))
+camera = Camera( 'location', [0, 0, 25], 'look_at', [0, 0, 0] ) #camera location
+light = LightSource( [0,0,0], 'color', [1, 1, 1] ) #light location and angle/color
 
 
 
 class Orbitor:
-    def __init__(self, initPos, initVel, mass, size):
+    def __init__(self, initPos, initVel, mass, size, texture):
         self.position = initPos; #3 list
         self.velocity = initVel; #3 list
         self.mass = mass;
         self.size = size;
+        self.texture = texture
 
 orbitorList = []
-def addObject(position, velocity, mass, size):
-    orbitorList.append(Orbitor(position, velocity, mass, size))
+def addObject(position, velocity, mass, size, tex):
+    orbitorList.append(Orbitor(position, velocity, mass, size, tex))
 
 
 #ORBITOR LIST
 #add as many objects as you want here,
-#syntax is addObject(position, velocity, mass)
-#position and velocity are written as [x, y, z] 
-addObject([0, 0, 0], [0, 10 * sqrt(Gconst)/1000, 0], 1000, 2)
-addObject([10, 0, 0], [0, -10 * sqrt(Gconst), 0], 1, 0.1)
-#addObject([0, 10, 0], [0, 0, -1], 1)
-#addObject([0, -10, 0], [0, 0, 1], 1)
+#syntax is addObject(position, velocity, mass, size, texture)
+#position and velocity are written as [x, y, z]
 
+#Solving the gravity formula for circular orbit, we find that velocity = distance from sun times  the square root of G
+
+addObject([0, 0, 0], [0, 0, 0], 1000, 1, sunTex) #sun
+addObject([10, 0, 0], [0, -10 * sqrt(Gconst), 0], 1, 0.2, earthTex) #earth
+
+
+#Lagrange points, all with 0 mass for simplicity's sake.
+#Constants are distance from small object orbit. Calculated by using the equations from https://en.wikipedia.org/wiki/Lagrange_point, M1 = 1000, M2 = 1, R = 10, solving the quintic for r using wolfram alpha.
+#speeds are calculated by using the same angular velocity as the earth object
+
+#L1 
+l1const = 0.67691010460029440734
+
+addObject([10 - l1const, 0, 0], [0, - (10 - l1const) * sqrt(Gconst), 0], 0, 0.2, lagTex) 
+
+#L2
+l2const = 0.70891951508136296748
+
+addObject([10 + l2const, 0, 0], [0, - (10 + l2const) * sqrt(Gconst), 0], 0, 0.2, lagTex)
+
+#L3
+l3const = 0.0058275063696938076265
+
+addObject([-(10 - l3const), 0, 0], [0, (10 - l3const) * sqrt(Gconst), 0], 0, 0.2, lagTex)
+
+
+#L4
+
+#Equilateral triangle of the three objects, computed by 30 - 60 - 90 triangle. Same speed as inner object of course
+
+addObject([5, 5 * sqrt(3), 0], [(sqrt(3)/2) * (10) * sqrt(Gconst), (1/2) * (-10) * sqrt(Gconst), 0], 0, 0.2, lagTex)
+
+#L5
+
+addObject([5, -5 * sqrt(3), 0], [(sqrt(3)/2) * (-10) * sqrt(Gconst), (1/2) * (-10) * sqrt(Gconst), 0], 0, 0.2, lagTex)
 
 
 #simulation code below
@@ -99,7 +136,7 @@ def stepDiffeq():
                         current.velocity[i] += vector[i] * acceleration
                         #velocity incremented, we done
     for current in orbitorList:
-        print("New position ", current.position) #debug print for fun and profit
+        print("New position ", current.position, sqrt(current.position[0] ** 2 + current.position[1] ** 2 + current.position[2] ** 2)) #debug print for fun and profit
     return
 
 
@@ -109,7 +146,7 @@ def createScene():
     #add all the objects
 
     for i in orbitorList:
-        objs.append(Sphere([i.position[0], i.position[1], i.position[2]], i.size, tex))
+        objs.append(Sphere([i.position[0], i.position[1], i.position[2]], i.size, i.texture))
     return Scene(camera, objects=objs)
 
 def renderScene(sn):
